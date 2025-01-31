9693b13ee922230e7d68c37f12cfcdcf70dbad39
sameerali <sameeraly240@gmail.com>
2025-01-31 13:02:36 +0500

    added

--- src/App.jsx
+++ src/App.jsx
@@ -26,34 +26,40 @@
 
 
 
+// import "./App.css";
+// import Card from "./componet/card"
+// function App() {
+//   return<>
+//   <Card/>
+  
+//   </>
+  
+// }
+// export default App
 import "./App.css";
-import Card from "./componet/card"
+import Card from "./componet/card";
+import Image from "./assets/MyImage.png"
+import Images from "./assets/smartphone.jpg";
 function App() {
   return<>
-  <Card/>
+  <Card header={"The working of laptop"}/>
+  <Card para={"A working laptop is a modern-day lifeline for productivity, offering the flexibility to work, study, or create from virtually anywhere. Its portability makes it the perfect companion for those on the go, allowing for quick access to information, communication, and digital tools. Whether it’s for a professional task or a personal project, a reliable laptop helps keep tasks on track, and with the right software, it can be a powerhouse of innovation and efficiency."}/>
+  <Card button={"click me"}/>
+  <img src={Image} alt="image" srcset=""  style={{width:"80%", height:"600px"}} />
   
+  
+  <Card header={"The working of mobile phone"}/>
+  <Card para={"A mobile phone works by using a combination of hardware and software to perform functions like making calls, sending messages, browsing the internet, and running application"}/>
+  <img src={Images} alt="image" srcset=""  style={{width:"80%", height:"600px" }} />
+  <Card button={"click me"}/>
   </>
   
 }
-export default App
+export default App;
 
 
 
-// src/App.js
-// import React from "react";
-// import Header from "./componet/heading";
-// import "./App.css";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <Header />
-//       <p>This is a simple React app.</p>
-//     </div>
-//   );
-// }
-
-// export default App;
 
 
 
---
+++ src/componet/Image/image.jsx
@@ -0,0 +1,7 @@
+function Image({ src }) {
+    return(
+        <img src={src} alt="Image" />
+    );
+}
+
+export default Image;
