af0605924432aa8e0f3ed6e09672eff5b39b771a
sameerali <sameeraly240@gmail.com>
2025-02-02 17:22:28 +0500

    added

--- src/App.jsx
+++ src/App.jsx
@@ -36,24 +36,37 @@
   
 // }
 // export default App
-import {faMagnifyingGlass} from '@fortawesome/fontawesome-svg-core'
-import "./App.css";
+// import {faMagnifyingGlass} from '@fortawesome/fontawesome-svg-core'
+// import "./App.css";
+// import Card from "./componet/card";
+// import Image from "./assets/MyImage.png"
+// import Images from "./assets/smartphone.jpg";
+// function App() {
+//   return<>
+//   <Card header={"The working of laptop"}/>
+//   <Card para={"A working laptop is a modern-day lifeline for productivity, offering the flexibility to work, study, or create from virtually anywhere. Its portability makes it the perfect companion for those on the go, allowing for quick access to information, communication, and digital tools. Whether it’s for a professional task or a personal project, a reliable laptop helps keep tasks on track, and with the right software, it can be a powerhouse of innovation and efficiency."}/>
+//   <Card button={"click me"} btnIcon={faMagnifyingGlass}/>
+//   <img src={Image} alt="image" srcset=""  style={{width:"80%", height:"600px"}} />
+  
+  
+//   <Card header={"The working of mobile phone"}/>
+//   <Card para={"A mobile phone works by using a combination of hardware and software to perform functions like making calls, sending messages, browsing the internet, and running application"}/>
+//   <img src={Images} alt="image" srcset=""  style={{width:"80%", height:"600px" }} />
+//   <Card button={"click me"}/>
+//   </>
+  
+// }
+// export default App;
+
+
 import Card from "./componet/card";
-import Image from "./assets/MyImage.png"
-import Images from "./assets/smartphone.jpg";
+import "./App.css";
 function App() {
   return<>
-  <Card header={"The working of laptop"}/>
-  <Card para={"A working laptop is a modern-day lifeline for productivity, offering the flexibility to work, study, or create from virtually anywhere. Its portability makes it the perfect companion for those on the go, allowing for quick access to information, communication, and digital tools. Whether it’s for a professional task or a personal project, a reliable laptop helps keep tasks on track, and with the right software, it can be a powerhouse of innovation and efficiency."}/>
-  <Card button={"click me"} btnIcon={faMagnifyingGlass}/>
-  <img src={Image} alt="image" srcset=""  style={{width:"80%", height:"600px"}} />
+    <Card/>
   
+   </>
   
-  <Card header={"The working of mobile phone"}/>
-  <Card para={"A mobile phone works by using a combination of hardware and software to perform functions like making calls, sending messages, browsing the internet, and running application"}/>
-  <img src={Images} alt="image" srcset=""  style={{width:"80%", height:"600px" }} />
-  <Card button={"click me"}/>
-  </>
   
 }
 export default App;
