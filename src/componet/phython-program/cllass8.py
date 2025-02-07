f6cc088b297287e46d83e406bacfa53c7b29c317
sameerali306 <sameeraly240@gmail.com>
2025-02-07 21:05:43 +0500

    include

---
+++ src/componet/phython-program/cllass8.py
@@ -0,0 +1,21 @@
+str1="my name is sameer ali.\nmy age is 20.\n.I complete my intermediate in 2024."
+print(str1)
+
+str1="sameer"
+str2="ali"
+final_str=str1+str2
+print(final_str)
+print(len(final_str))
+ch=final_str[3]
+print(ch)
+str=final_str[1:]
+print(str)
+stre=final_str[-2:-4]
+print(stre)
+
+word="i am sameer ali"
+print(word.endswith("li"))
+print(word.capitalize())
+print(word.replace("a","e"))
+print(word.find("r"))
+print(word.count("a"))
