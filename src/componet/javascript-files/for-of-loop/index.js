882cc35f8a1366d806b76c85e171ad4b06c8c2f6
sameerali306 <sameeraly240@gmail.com>
2025-02-09 21:47:17 +0500

    inter

---
+++ src/componet/javascript-files/for-of-loop/index.js
@@ -0,0 +1,8 @@
+const fruits=["apple","orange","mange","banana","appricote"]
+const fruits2=[]
+for(let fruit of fruits){
+    console.log(fruit);
+    fruits2.push(fruit.toUpperCase())
+    
+}
+console.log(fruits2);
