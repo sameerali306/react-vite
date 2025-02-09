2daa7d456135d2bc82229af44a577be5f8a369da
sameerali306 <sameeraly240@gmail.com>
2025-02-09 20:51:48 +0500

    include

---
+++ src/componet/phython-program/class10.py
@@ -0,0 +1,7 @@
+light=input("look for traffic signal tower and enter color :")
+if(light=="green"):
+    print("now go")
+elif(light=="red"):
+    print("please stop")
+elif(light=="yellow"):
+    print("please look and go")
