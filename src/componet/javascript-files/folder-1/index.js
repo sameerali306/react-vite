310452d946e6670064c65574a86b615b63b76ffd
sameerali306 <sameeraly240@gmail.com>
2025-02-09 21:21:23 +0500

    include

---
+++ src/componet/javascript-files/folder-1/index.js
@@ -0,0 +1,3 @@
+document.getElementById("greetButton").addEventListener("click", function() {
+    document.getElementById("greetingMessage").textContent = "Hello! Welcome to the world of JavaScript!";
+});
