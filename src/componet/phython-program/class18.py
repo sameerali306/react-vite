95b8876c2d269133ceaef58c1b61e1c3ddc9d057
sameerali306 <sameeraly240@gmail.com>
2025-02-10 22:12:32 +0500

    include

---
+++ src/componet/phython-program/class18.py
@@ -0,0 +1,17 @@
+marks=[40,60,90,98.9,30]
+print(marks)
+print(len(marks))
+print(marks[0])
+print(marks[1])
+print(marks[2])
+print(marks[3])
+#in phython we can store different type of data
+mixed=["karachi",45,45.4]
+print(mixed)
+print(mixed[0])
+print(mixed[1])
+print(mixed[2])
+mixed[0]="lahore"
+print(mixed)
+print(mixed[0:2])
+print(mixed[-3:-2])
