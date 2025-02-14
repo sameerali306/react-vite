8ad77c9dce8559f519efe354b88e49e0fdb94723
sameerali306 <sameeraly240@gmail.com>
2025-02-14 23:02:32 +0500

    add

---
+++ src/componet/phython-program/class24.py
@@ -0,0 +1,39 @@
+count=5
+while count>=1:
+    print(count)
+    count-=1
+    print("count ended")
+
+
+number=1
+while number<=100:
+    print(number)
+    number=number+1
+    print("number are print successfully ")
+
+minunumber=100
+while minunumber>=1:
+        print(minunumber)
+        minunumber=minunumber-1
+
+
+print("minus number are print successfully")
+
+i=1
+while i<=10:
+    print(3*i)
+    i=i+1
+print("3 table is print successfully")  
+i=1
+while i<=10:
+    print(5*i)  
+    i+=1
+print("5 table is print successfully")
+
+
+n=int(input("Enter youtr number to find table :"))
+i=1
+while i<=10:
+    print(n*i)
+    i +=1
+print("your neter number table is print successfully")    
