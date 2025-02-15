8e929d76cfb3a8c88a86e662911c570bfe2ac1fa
sameerali306 <sameeraly240@gmail.com>
2025-02-15 21:27:16 +0500

    enter

---
+++ src/componet/javascript-files/object inside array/index.js
@@ -0,0 +1,18 @@
+const users=[
+    {userId:1 , userName:"sameer", Gender:"male"},
+    {userId:2 , userName:"faria", Gender:"female"},
+    {userId:3 , userName:"nadia", Gender:"female"},
+    {userId:4 , userName:"shahzain", Gender:"male"},
+    
+]
+// console.log(user);
+for (let user of users) {
+    console.log(user.userId);
+    console.log(user.userName);
+    console.log(user.Gender);
+    
+    
+    
+    
+}
+
