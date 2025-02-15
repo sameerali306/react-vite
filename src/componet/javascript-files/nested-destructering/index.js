c3cf1a334a1b18ba55b176921b683b4e8367ad55
sameerali306 <sameeraly240@gmail.com>
2025-02-15 21:54:30 +0500

    main

---
+++ src/componet/javascript-files/nested-destructering/index.js
@@ -0,0 +1,18 @@
+const users=[
+    {userId:"1", userName:"sameer ali", Gender:"male"},
+    {userId:"2", userName:"faria", Gender:"female"},
+    {userId:"3", userName:"zavian", Gender:"male"},
+    
+]
+const[{userId},{Gender},{userName:user3firstname}]=users
+// console.log(user1);
+// console.log(user2);
+// console.log(user3);
+console.log(userId);
+console.log(Gender);
+console.log(user3firstname);
+
+
+
+
+
