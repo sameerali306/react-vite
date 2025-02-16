622ea6928c209f0dfe30747693959f7aabe9ec66
sameerali306 <sameeraly240@gmail.com>
2025-02-16 23:18:02 +0500

    added

---
+++ src/componet/phython-program/class27.py
@@ -0,0 +1,15 @@
+# i=0
+# while i<5:
+#     if(i==3):
+#         i+=1
+#         continue
+#     print(i)
+#     i+=1
+
+i=0
+while i<10:
+    if(i%2==0):
+        i+=1
+        continue
+    print(i)
+    i+=1
