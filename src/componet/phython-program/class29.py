51de1326d49ae73c0ff09767a3db9cc1ed3df979
sameerali306 <sameeraly240@gmail.com>
2025-02-17 23:16:26 +0500

    branch

---
+++ src/componet/phython-program/class29.py
@@ -0,0 +1,39 @@
+# for i in range(10):
+#     print(i)
+# print("first fucation complete")
+# for i in range(2,20):
+#     print(i)    
+# print("second funcation also complete")    
+# for i in range (2,20,5):
+#     print(i)
+
+# for i in range (2,100,2):
+#     print(i)
+# print("Your Even number ARE PRINT SUCCESSFULLY")    
+# for num in range(1,100,2):
+#     print(num)
+# print("Your old number are print successfully")    
+
+# for i in range(0,51,5):
+#     print(i)
+# for i in range(100,0,-1):
+#     print(i)
+
+# num=int(input("enter your number"))
+# for i in range(1,11):
+#     print(num*i)
+
+# for i in range(4):
+#     pass
+# print("Your loop is empty")
+# n=5
+# sum=0
+# for i in range(1,n+1):
+#     sum +=i
+# print("the total is :",sum)    
+
+n=int(input("Enter a number to find sum :"))
+sum=0
+for i in range(1,1+n):
+    sum +=i
+print("your total is :",sum)    
