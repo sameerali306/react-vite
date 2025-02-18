e0f9a5af993fd1d393cac33cab0baaee0c272f08
sameerali306 <sameeraly240@gmail.com>
2025-02-18 21:37:28 +0500

    arrow

---
+++ src/componet/javascript-files/Arrow-funcation/index.js
@@ -0,0 +1,17 @@
+// const sum=(number1,number2,number3)=>{
+//     return number1+number2+number3
+// }
+// console.log(sum(13,13,2));
+
+const n=prompt("Enter a number")
+i=0
+const multiply=()=>{
+    for(i=0;i<=10;i++){
+        console.log(n*i);
+         
+        
+    }
+}
+const value= multiply()
+console.log(value);
+
