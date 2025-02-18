778aab3f78d88175d5fb2f7d1795b57d0368ab66
sameerali306 <sameeraly240@gmail.com>
2025-02-18 13:43:21 +0500

    include

--- src/App.jsx
+++ src/App.jsx
@@ -183,13 +183,40 @@
 // export default App
 
 
-import Input from "./componet/Ref/index2"
-function App() {
-  return(
-    <Input/>
-  )
+// import Input from "./componet/Ref/index2"
+// function App() {
+//   return(
+//     <Input/>
+//   )
  
 
   
+// }
+// export default App
+
+
+
+
+import { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  const [inputValue, setInputValue] = useState("");
+  const count = useRef(0);
+
+  useEffect(() => {
+    count.current = count.current + 1;
+  });
+
+  return (
+    <>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <h1>Render Count: {count.current}</h1>
+    </>
+  );
 }
 export default App
