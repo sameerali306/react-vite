859cad8bc0d6a6991dda99e4884672bcb8046257
sameerali306 <sameeraly240@gmail.com>
2025-02-26 12:36:13 +0500

    add

---
+++ src/componet/javascript-files/sort_method/index.js
@@ -0,0 +1,12 @@
+// sort method can sort a numbers according to tha asci code and see the asci code of first number
+const number=[1,2,4,6,7,9,12,31,21,41]
+number.sort()
+console.log(number);
+
+const num=[12,21,41,111,112,333,222,45,64,]
+num.sort()
+console.log(num);
+// in english the sort method can be done according to uppercase letter first and than and than alphabet 
+const alpa=["abc","sameer","hello","hi","Hi","ABC","Sameer"]
+alpa.sort()
+console.log(alpa);
